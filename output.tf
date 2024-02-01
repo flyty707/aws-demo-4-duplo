4258446bb6b32e5fa918cf7d3e4111d17a7cf3e1
flyty707 <flyty707@gmail.com>
2024-01-31 20:29:15 -0800

    Update output.tf
    bugfix

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "cluster_name" {
   value = module.eks.cluster_id
 }
 
-output "webapp_service_url" {
-  value = kubernetes_service.webapp.status[0].load_balancer.ingress[0].hostname
-}
+# output "webapp_service_url" {
+#   value = kubernetes_service.webapp.status[0].load_balancer.ingress[0].hostname
+# }
