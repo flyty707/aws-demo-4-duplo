5dc64505c299da2c9fca9eca33a7edf8312e7c09
flyty707 <flyty707@gmail.com>
2024-02-01 10:21:22 -0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,6 +2,7 @@ output "cluster_name" {
   value = module.eks.cluster_id
 }
 
+# Need cfg loadbalancer post cluster config
 # output "webapp_service_url" {
 #   value = kubernetes_service.webapp.status[0].load_balancer.ingress[0].hostname
 # }
